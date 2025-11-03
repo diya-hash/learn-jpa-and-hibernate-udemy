e06abc9adf7a037183fbe46689577e83a7201546
Diya Salam <sdiya.salam.github@gmail.com>
2025-11-02 23:17:07 -0500

    add

--- src/main/java/com/udemy/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/udemy/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -5,18 +5,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.udemy.learn_jpa_and_hibernate.course.Course;
+import com.udemy.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
 
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 
+//	@Autowired
+//	private CourseJdbcRepository repository;
 	@Autowired
-	private CourseJdbcRepository repository;
+	private CourseJpaRepository repository;
 	@Override
 	public void run(String... args) throws Exception {
 		repository.insert(new Course(1, "Learn AWS now!", "Udemy"));
 		repository.insert(new Course(2, "Learn azure now!", "Udemy"));
 		repository.insert(new Course(3, "Learn GCP now!", "Udemy"));
-		repository.deleteById(3);
+//		repository.deleteById(3);
 		System.out.println(repository.findById(1));
 	}
 	
---
+++ src/main/java/com/udemy/learn_jpa_and_hibernate/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,25 @@
+package com.udemy.learn_jpa_and_hibernate.course.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.learn_jpa_and_hibernate.course.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void insert(Course course) {
+		entityManager.merge(course);
+	}
+	public Course findById(long id) {
+		return entityManager.find(Course.class, entityManager);
+	}
+}
