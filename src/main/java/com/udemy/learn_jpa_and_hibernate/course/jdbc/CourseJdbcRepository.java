2ababd5ea4700a36c414bfd67dd390c47259eced
Diya Salam <sdiya.salam.github@gmail.com>
2025-11-02 21:45:37 -0500

    add

---
+++ src/main/java/com/udemy/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,19 @@
+package com.udemy.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+	@Autowired
+	private JdbcTemplate springJdbcTemplate;
+	private static String INSERT_QUERY =
+			"""
+				INSERT INTO COURSE(id, name, author)
+				values(1, 'Learn AWS', 'in28minutes')
+			""";
+	public void insert() {
+		springJdbcTemplate.update(INSERT_QUERY);
+	}
+}
